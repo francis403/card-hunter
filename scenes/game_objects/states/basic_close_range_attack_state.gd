0b12b6e5f948e07a22a8e19186f3e251741194f5
francis403 <frainsmallletters@gmail.com>
2025-03-03 11:29:03 +0000

    Monster only attacks if within range

--- scenes/game_objects/states/basic_close_range_attack_state.gd
+++ scenes/game_objects/states/basic_close_range_attack_state.gd
@@ -1,10 +1,12 @@
 extends State
-class_name BasicCloseRangeAttackState
+class_name MoveSpeedToPlayerAndRadiusAttack
 
 var target: PlayerPiece
 var monster: MonsterPiece
 
 var attack_behaviour: MonsterBehaviourController = null
+
+@export var range: int = 1
 
 func enter_state():
 	target = BattleController.get_player()
@@ -19,10 +21,21 @@ func exit_state():
 func do_state_action():
 	if not monster_behaviour:
 		return
+		
 	if movement_behaviour_node:
 		movement_behaviour_node.do_movement()
+	
+	var distance_to_player = BattleController.distance_between_tiles(
+		monster.next_move if monster.next_move else monster._tile,
+		target._tile
+	)
+	
+	if distance_to_player > 1:
+		BattlemapSignals.clear_attack_highlight_tiles.emit()
+		return
+	
 	if monster_behaviour.attack_behaviour:
-		# TODO: should this be just like the movement behaviour??				
+		# TODO: should this be just like the movement behaviour??
 		attack_behaviour.play_monster_behaviour(
 			monster_behaviour
 		)
