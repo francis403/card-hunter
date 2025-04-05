348adeb2071d15c04152e1f91d6f20196b782b57
francis403 <frainsmallletters@gmail.com>
2025-04-05 11:42:27 +0200

    add tile hightlight config and improve radius attack

--- scenes/managers/battlemap_generator_manager/grid_highlight_manager.gd
+++ scenes/managers/battlemap_generator_manager/grid_highlight_manager.gd
@@ -19,7 +19,10 @@ func _on_highlight_tiles_signal(
 	range: int,
 	area_type: Constants.AreaType
 ):
-	highlight_tiles(source_tile, range, area_type)
+	var config = TileHighlightConfig.new()
+	config.area_type = area_type
+	config.range = range
+	highlight_tiles(source_tile, config)
 	
 	
 ## Highlight all movable tiles 
@@ -28,7 +31,11 @@ func _on_hightlight_move_tiles_signal(
 	range: int,
 	area_type: Constants.AreaType
 ):
-	highlight_tiles(source_tile, range, area_type, false, true)	
+	var config = TileHighlightConfig.new()
+	config.area_type = area_type
+	config.range = range
+	config.ignore_occupied_tiles = true
+	highlight_tiles(source_tile, config)
 
 ## Highlights all tiles as attacked
 func _on_highlight_attacked_tiles_signal(
@@ -36,29 +43,28 @@ func _on_highlight_attacked_tiles_signal(
 	range: int,
 	area_type: Constants.AreaType
 ):
-	highlight_tiles(source_tile, range, area_type, true, false)	
+	var config = TileHighlightConfig.new()
+	config.area_type = area_type
+	config.range = range
+	config.is_tile_attacked = true
+	highlight_tiles(source_tile, config)	
 
 ## Highlights all tiles
+## TODO: need to pass a tile highlight config object
 func highlight_tiles(
 	source_tile: Tile,
-	range: int,
-	area_type: Constants.AreaType,
-	is_tile_attacked: bool = false,
-	ignore_occupied_tiles: bool = false
+	config: TileHighlightConfig
 ):
+	var area_type: Constants.AreaType = config.area_type
 	if area_type == Constants.AreaType.RADIUS:
 		highligh_tiles_radius(
 			source_tile,
-			range,
-			is_tile_attacked,
-			ignore_occupied_tiles
+			config
 		)
 	elif area_type == Constants.AreaType.CROSS:
 		highligh_tiles_cross(
 			source_tile,
-			range,
-			is_tile_attacked,
-			ignore_occupied_tiles
+			config
 		)
 	elif area_type == Constants.AreaType.SPECIFIC:
 		print("TODO: specific movement")
@@ -67,63 +73,60 @@ func highlight_tiles(
 	elif area_type == Constants.AreaType.LINE:
 		highligh_tiles_line(
 			source_tile,
-			range,
-			is_tile_attacked,
-			ignore_occupied_tiles
+			config
 		)
-
-# TODO: how can I do this well?
+	
 func highligh_tiles_radius(
 	source_tile: Tile,
-	radius: int,
-	is_tile_attacked: bool = false,
-	ignore_occupied_tiles: bool = false
+	config: TileHighlightConfig
 ):
-	var x = source_tile._x_position
-	var y = source_tile._y_position
-	_make_tile_clickable(x + 1, y, is_tile_attacked, ignore_occupied_tiles)
-	_make_tile_clickable(x - 1, y, is_tile_attacked, ignore_occupied_tiles)
-	_make_tile_clickable(x, y + 1, is_tile_attacked, ignore_occupied_tiles)
-	_make_tile_clickable(x, y - 1, is_tile_attacked, ignore_occupied_tiles)
-	_make_tile_clickable(x - 1, y - 1, is_tile_attacked, ignore_occupied_tiles)	
-	_make_tile_clickable(x + 1, y + 1, is_tile_attacked, ignore_occupied_tiles)
-	_make_tile_clickable(x - 1, y + 1, is_tile_attacked, ignore_occupied_tiles)
-	_make_tile_clickable(x + 1, y - 1, is_tile_attacked, ignore_occupied_tiles)
-	
+	var x: int = source_tile._x_position
+	var y: int = source_tile._y_position
+	var radius = config.range
+	for radius_x in range(-radius, radius + 1):
+		for radius_y in range(-radius, radius + 1):
+			var tile_x = x + radius_x
+			var tile_y = y + radius_y
+			var radius_distance: int = abs(radius_x) + abs(radius_y)
+			if config.ignore_origin and radius_distance == 0:
+				continue
+			## TODO: do corners count for the radius?
+			#if abs(radius_x) + abs(radius_y) <= radius:
+			_make_tile_clickable(tile_x, tile_y, config)
+
 func highligh_tiles_cross(
 	source_tile: Tile,
-	line_length: int, 
-	is_tile_attacked: bool = false,
-	ignore_occupied_tiles: bool = false
+	config: TileHighlightConfig
 ):
 	var x = source_tile._x_position
 	var y = source_tile._y_position
+	var line_length: int = config.range
 	for i in range(1, line_length + 1):
-		_make_tile_clickable(x + i, y, is_tile_attacked, ignore_occupied_tiles)
-		_make_tile_clickable(x - i, y, is_tile_attacked, ignore_occupied_tiles)
-		_make_tile_clickable(x, y + i, is_tile_attacked, ignore_occupied_tiles)
-		_make_tile_clickable(x, y - i, is_tile_attacked, ignore_occupied_tiles)
+		_make_tile_clickable(x + i, y, config)
+		_make_tile_clickable(x - i, y, config)
+		_make_tile_clickable(x, y + i, config)
+		_make_tile_clickable(x, y - i, config)
 	
 ## TODO: this will need the player to hover over where he wants the attack to go
 func highligh_tiles_line(
 	source_tile: Tile,
-	line_length: int, 
-	is_tile_attacked: bool = false,
-	ignore_occupied_tiles: bool = false
+	config: TileHighlightConfig
 ):
 	var x = source_tile._x_position
 	var y = source_tile._y_position
+	var line_length = config.range
 	for i in range(1, line_length + 1):
-		_make_tile_clickable(x + i, y, is_tile_attacked, ignore_occupied_tiles)
+		_make_tile_clickable(x + i, y, config)
 	
 
 func _make_tile_clickable(
 	x: int,
 	y: int,
-	is_tile_attacked: bool = false,
-	ignore_occupied_tiles: bool = false
+	config: TileHighlightConfig
 ):
 	var tile: Tile = BattleController.get_tile(x, y)
+	var ignore_occupied_tiles: bool = config.ignore_occupied_tiles
+	var is_tile_attacked: bool = config.is_tile_attacked
 	if not tile:
 		return
 	if tile.piece_in_tile && ignore_occupied_tiles:
---
+++ scripts/configuration/tile_highlight_configuration/tile_highlight_configuration.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name TileHighlightConfig
+
+var range: int = 1
+var area_type: Constants.AreaType = Constants.AreaType.INHERIT
+var is_tile_attacked: bool = false
+var ignore_occupied_tiles: bool = false
+var ignore_origin: bool = true
+var ignore_coners: bool = false
