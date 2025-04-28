de6da718dd647224f4d2c2ded8b55e85ccd532cd
francis403 <frainsmallletters@gmail.com>
2025-04-28 23:08:42 +0200

    flash on player hit

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -6,6 +6,7 @@ const PLAYER_HIT_1 = preload("res://assets/sound/sound_effects/player_hit_1.mp3"
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 @onready var status_effect_container: StatusEffectContainer = $StatusEffectContainer
 @onready var status_effects_ui: StatusEffectUI = $StatusEffectsUI
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _ready() -> void:
 	super._ready()
@@ -36,6 +37,21 @@ func remove_status(status_id: String):
 	status_effect_container.remove_status(status_id)
 
 func apply_damage(damage: int):
+	_play_hit_flash()
 	audio_stream_player.stream = PLAYER_HIT_1
 	audio_stream_player.play()
 	super.apply_damage(damage)
+
+func _play_hit_flash():
+	if sprite_2d.material:
+		var tween = create_tween()
+		tween.tween_method(
+			set_flash_modifier,
+			1.0,
+			0.0,
+			0.2
+		)
+
+func set_flash_modifier(value: float) -> void:
+	if sprite_2d.material:
+		sprite_2d.material.set_shader_parameter("flash_modifier", value)
