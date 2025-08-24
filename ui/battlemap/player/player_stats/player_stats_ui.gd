25f06ee8ddff92691f884b186c27d6772d744e4a
francis403 <frainsmallletters@gmail.com>
2025-08-24 11:01:37 +0200

    feature-7: improve player stats ui

--- ui/battlemap/player/player_stats/player_stats_ui.gd
+++ ui/battlemap/player/player_stats/player_stats_ui.gd
@@ -22,25 +22,28 @@ func _ready() -> void:
 	BattlemapSignals.monster_hp_changed.connect(_on_monster_health_changed)
 	_initialize_player_stats()
 
-func _on_player_stamina_changed(current_stamina: int):
-	# Animate the change
-	var tween = create_tween()
-	tween.tween_property(stamina_progress_bar, "value", current_stamina, 0.2)
-	# Brief highlight effect
-	var highlight_tween = create_tween()
-	highlight_tween.tween_property(stamina_progress_bar, "modulate", Color(1.2, 1.2, 1.5, 1), 0.1)
-	highlight_tween.tween_property(stamina_progress_bar, "modulate", Color.WHITE, 0.2)
-	player_stamina_label.text = str(current_stamina) + "/" + str(50)
+func _on_player_stamina_changed(_current_stamina: int):
+	_animate_progress_bar(stamina_progress_bar, _current_stamina)
+	player_stamina_label.text = str(_current_stamina) + "/" + str(50)
 
-func _on_player_health_changed(current_health: int):
-	player_health_label.text = str(current_health) + "/" + str(100)
-	health_progress_bar.value = current_health
-	var percentage: float = current_health/100
+func _on_player_health_changed(_current_health: int):
+	_animate_progress_bar(health_progress_bar, _current_health)
+	player_health_label.text = str(_current_health) + "/" + str(100)
+	var percentage: float = _current_health/100
 	if percentage < _critical_health_range:
 		_add_critical_health_effect()
 	elif _critical_health_effect_tween and _critical_health_effect_tween.is_running():
 		_critical_health_effect_tween.kill()
 		
+func _animate_progress_bar(_progress_bar: ProgressBar, value: int):
+	# Animate the change
+	var tween = create_tween()
+	tween.tween_property(_progress_bar, "value", value, 0.2)
+	# Brief highlight effect
+	var highlight_tween = create_tween()
+	highlight_tween.tween_property(_progress_bar, "modulate", Color(1.2, 1.2, 1.5, 1), 0.1)
+	highlight_tween.tween_property(_progress_bar, "modulate", Color.WHITE, 0.2)
+
 func _add_critical_health_effect():
 	if _critical_health_effect_tween and _critical_health_effect_tween.is_running():
 		return
@@ -64,4 +67,4 @@ func _on_monster_health_changed(new_hp: int, max_hp: int):
 	monster_hp_progress_bar.value = progress_bar_value
 
 func _initialize_player_stats():
-	player_health_label.text = "HP: " + str(PlayerController.current_player_health)
+	player_health_label.text = str(PlayerController.current_player_health) + "/" + str(100)
